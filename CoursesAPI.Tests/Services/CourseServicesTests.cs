0ee0a11b5a0433892d4b2b0759accfe2e9026084
Stebbi30 <Stebbi30@gmail.com>
2014-10-05 18:13:38 +0000

    testapi

--- CoursesAPI.Tests/Services/CourseServicesTests.cs
+++ CoursesAPI.Tests/Services/CourseServicesTests.cs
@@ -1,25 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoursesAPI.Services.Services;
+using CoursesAPI.Tests.MockObjects;
+using CoursesAPI.Services.Models.Entities;
+
 
 namespace CoursesAPI.Tests.Services
 {
 	[TestClass]
 	public class CourseServicesTests
 	{
+        private CoursesServiceProvider _service;
+        private MockUnitOfWork<MockDataContext> _uow;
+
 		[TestInitialize]
 		public void Setup()
 		{
+            _uow = new MockUnitOfWork<MockDataContext>();
+            _service = new CoursesServiceProvider(_uow);
 			// TODO: code which will be executed before each test!
 		}
 
+
 		[TestMethod]
-		public void TestMethod1()
+		public void TeacherListInCourse()
 		{
 			// Arrange:
+            const int courseId = 1;
+            var teachers = new List<Person>
+            {
+                new Person
+                {
+                    ID = 1,
+                    SSN = "2701903249",
+                    Name = "Stefán",
+                    Email = "stebbi30@gmail.com"
+                }
+            };
 
+            _uow.SetRepositoryData(teachers);
+
+            var teacherRegistration = new List<TeacherRegistration> { };
+            _uow.SetRepositoryData(teacherRegistration);
 			// Act:
+            var resault = _service.GetCourseTeachers(courseId);
 
 			// Assert:
+            Assert.AreEqual(0, resault);
 		}
+
+        [TestMethod]
+        public void CoursesGetStudentListInInvalidCourse()
+        {
+            // Arrange:
+
+            // Act:
+
+            // Assert:
+        }
+
+        [TestMethod]
+        public void CoursesGetStudentListInDeregisteredStudents()
+        {
+            // Arrange:
+
+            // Act:
+
+            // Assert:
+        }
 	}
 }
