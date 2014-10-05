7031eaffd58416b2b78a1d7d3f65de1bca825a22
Stebbi30 <Stebbi30@gmail.com>
2014-10-05 19:54:59 +0000

    kjhfk

--- CoursesAPI.Tests/Services/CourseServicesTests.cs
+++ CoursesAPI.Tests/Services/CourseServicesTests.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoursesAPI.Services.Services;
 using CoursesAPI.Tests.MockObjects;
 using CoursesAPI.Services.Models.Entities;
+using CoursesAPI.Models;
 
 
 namespace CoursesAPI.Tests.Services
@@ -24,40 +25,72 @@ namespace CoursesAPI.Tests.Services
 
 
 		[TestMethod]
-		public void TeacherListInCourse()
+		public void CheckGrade()
 		{
 			// Arrange:
             const int courseId = 1;
-            var teachers = new List<Person>
+            const String ssn = "2701903249";
+
+            var persons = new List<Person>
             {
                 new Person
                 {
-                    ID = 1,
                     SSN = "2701903249",
-                    Name = "Stefán",
-                    Email = "stebbi30@gmail.com"
+                    Name = "Stefan",
+                    Email = "dsadsa",
+
+                }
+            };
+
+            var teachers = new List<TeacherRegistration>
+            {
+                new TeacherRegistration
+                {
+                    SSN = "2701903249",
+                    CourseInstanceID = 1,
+                    Type = 1,
+
+                }
+            };
+
+            var courseInstances = new List<CourseInstance>
+            {
+                new CourseInstance
+                {
+                    ID = 1,
+                    CourseID = "dsddsa",
+                    SemesterID = "dsa",
                 }
             };
 
             _uow.SetRepositoryData(teachers);
+            _uow.SetRepositoryData(persons);
+            _uow.SetRepositoryData(courseInstances);
 
-            var teacherRegistration = new List<TeacherRegistration> { };
-            _uow.SetRepositoryData(teacherRegistration);
 			// Act:
-            var resault = _service.GetCourseTeachers(courseId);
-
+            var list = _service.GetCourseTeachers(courseId);
+            Person result = list[0];
+            
 			// Assert:
-            Assert.AreEqual(0, resault);
+            Assert.AreEqual(ssn, result.SSN);
 		}
 
         [TestMethod]
-        public void CoursesGetStudentListInInvalidCourse()
+        public void AddProject()
         {
             // Arrange:
+            var projectGroups = new List<ProjectGroup> { };
+            _uow.SetRepositoryData(projectGroups);
 
+            AddProjectGroupDTO pg = new AddProjectGroupDTO{
+                Name = "TestP",
+                GradesProjectCount = 2,
+            };
             // Act:
-
+            var result = _service.AddProjectGroup(pg);
+            var pgResult = _uow.GetRepository<ProjectGroup>();
             // Assert:
+            //Assert.AreEqual(1, );
         }
 
         [TestMethod]
